f444b69e19c09cdf9245d9b0b57659861781c26f
Tony Nguyen <tonyviet87@gmail.com>
2016-04-16 13:09:46 -0400

    Added registration routes an auth_token column for users table

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  resources: :registrations, only: [:create]
+
+  # post "/registrations", to: "registrations#create"
+  
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
