5c97906f10f6eeebd2ee2bef1f6cc7b66c403122
Jennifer Ford <jenncford@gmail.com>
2016-04-16 18:34:15 -0400

    vote migration update user_id, candidate_id, vote

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416183431) do
+ActiveRecord::Schema.define(version: 20160416223308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,11 @@ ActiveRecord::Schema.define(version: 20160416183431) do
   end
 
   create_table "votes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "user_id"
+    t.integer  "candidate_id"
+    t.integer  "vote"
   end
 
 end
