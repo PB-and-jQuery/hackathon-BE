2eb4ce1c8ee672c7adce0f08db503fec3f4317e2
Tony Nguyen <tonyviet87@gmail.com>
2016-04-16 11:47:40 -0400

    Add User Model

---
+++ hackathon-BE/app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+	has_secure_password
+	validates_presence_of :password, :email
+	validates :password, length: { minimum: 8 }
+	validates :email, format: {
+              with: /.+@.+\..+/,  message: "Must have an @ symbol and a period."
+           	  }
+    # has_many :posts
+    # has_many :comments
+    # has_many :votes
+    # has_many :voted_posts, through: :votes, source: :post
+    # has_many :voted_comments, through: :votes, source: :comment
+end
