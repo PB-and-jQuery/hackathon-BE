871c08e62ee5c71aee946b6ea5dc9b3b889a419b
Tony Nguyen <tonyviet87@gmail.com>
2016-04-16 12:52:55 -0400

    Added application, login and registrations controller.. also add user model

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,17 @@
 class RegistrationsController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    @user.ensure_auth_token
+    if @user.save
+      render json: { user: @user.as_json(only: [:first_name, :last_name, :email, :auth_token ]) },
+             status: :created
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
 
+  def user_params
+    params.permit(:email, :password, :first_name, :last_name, :voter_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,26 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates_presence_of :password, :email
 	validates :password, length: { minimum: 8 }
-	validates :email, format: {
-              with: /.+@.+\..+/,  message: "Must have an @ symbol and a period."
-           	  }
+  validates :email, presence: true, uniqueness: true,
+    format: {
+      with: /.+\@.+\..+/,
+      message: "must have an @ symbol and period"
+    }
+  validates :auth_token, presence: true
+  
+  def ensure_auth_token
+    unless self.auth_token
+      self.auth_token = User.generate_token
+    end
+  end
+
+  def self.generate_token
+    token = SecureRandom.hex
+    while User.exists?(auth_token: token)
+      token = SecureRandom.hex
+    end
+    token
+  end
     # has_many :posts
     # has_many :comments
     # has_many :votes
