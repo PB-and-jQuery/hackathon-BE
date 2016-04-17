ae34760e100babfa764f67d8680076b9c21a766a
Tony Nguyen <tonyviet87@gmail.com>
2016-04-17 13:18:25 -0400

    tcp connection error, loop error

--- lib/client.rb
+++ lib/client.rb
@@ -3,13 +3,13 @@ require "pry"
 require "json"
 
 class Client
-  include HTTparty
+  include HTTParty
 
   def vote(auth_token)
   	# user = User.all
   	candidate_id = rand(16..18)
     Client.post("/candidates/#{candidate_id}/votes", query: {
- #   	user_id: params["user_id"],
+   	# user_id: rand(1..515),
     	vote: 1
     	}, headers: { "Auth-Token": auth_token})
   end
@@ -29,5 +29,5 @@ user_tokens = ["f06c50e2ccc6019ab1d8865236d8a9c8",
 
 user_tokens.each do |token|
 	client.vote(token)
-	sleep 0.5
+	sleep rand(0.1..1)
 end
