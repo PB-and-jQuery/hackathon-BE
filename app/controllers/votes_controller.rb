0d41fff6818d0d02d35efe74cde1b1f7f5794dfc
Jennifer Ford <jenncford@gmail.com>
2016-04-17 09:23:21 -0400

    undid changes in last commit that I didn't want to

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,8 +2,9 @@ class VotesController < ApplicationController
   before_action :authenticate!, execpt: [:index]
 
   def create
-    @vote = current_user.votes.new(candidate_id: params[:candidate_id],
-                                  vote: params[:vote])
+    @vote = Vote.new(candidate_id: params[:candidate_id],
+                     user_id: params[:user_id],
+                     vote: params[:vote])
     if @vote.save
       status: :created
     else
